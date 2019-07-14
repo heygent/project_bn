e119b58d19b2f43426196ee4b898750b610a48b0
heygent <heygent@users.noreply.github.com>
2019-07-14 10:44:01 +0200

    Aggiunge test per mpe

--- project_bn/tests/test_inferences.py
+++ project_bn/tests/test_inferences.py
@@ -22,3 +22,18 @@ def test_map(Ms, e, bn, expected_map_value, expected_assignments):
     assignments, map_value = map_ask(Ms, e, bn)
     assert isclose(map_value, expected_map_value)
     assert assignments == expected_assignments
+
+
+@pytest.mark.parametrize(
+    "e,bn,expected_mpe_value,expected_assignments",
+    [
+        (
+            dict(GrassWet=True, RoadWet=False),
+            make_sprinkler_plus(),
+            0.6843176151770903,
+            dict(Season="summer", Sprinkler=True, Cloudy=False, Rain="no"),
+        )
+    ],
+)
+def test_mpe(e, bn, expected_mpe_value, expected_assignments):
+    pytest.fail("Not implemented")
