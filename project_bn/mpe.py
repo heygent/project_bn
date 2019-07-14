a648ddb52458dec4be8740eb2d9cb5a18d7cdd3b
acptIA <acpt1994@gmail.com>
2019-07-14 10:43:50 +0200

    mpe

---
+++ project_bn/mpe.py
@@ -0,0 +1,28 @@
+from aima_probability import *
+
+
+def mpe(X, e, bn):
+    """Compute bn's P(X|e) by variable elimination. [Figure 14.11]
+    >>> elimination_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary
+    ...  ).show_approx()
+    'False: 0.716, True: 0.284'"""
+    assert X not in e, "Query variable must be distinct from evidence"
+    factors = []
+    for var in reversed(bn.variables):
+        factors.append(make_factor(var, e, bn))
+        if is_hidden(var, X, e):
+            factors = max_out(var, factors, bn)
+    return pointwise_product(factors, bn).normalize()
+
+
+def max_out(var, factors, bn):
+    """Eliminate var from all factors by summing over its values."""
+    result, var_factors = [], []
+    for f in factors:
+        if var in f.variables:
+            var_factors.append(f)
+        else:
+            result.append(f)
+    result.append(pointwise_product(var_factors, bn).max_out(var, bn))
+    return result
+
