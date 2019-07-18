c9554db8e43981862a0cd954a848f5a836df93ba
acptIA <acpt1994@gmail.com>
2019-07-18 20:34:41 +0200

    che ne so

--- project_bn/main.py
+++ project_bn/main.py
@@ -1,6 +1,8 @@
 from aima_probability import *
 from project_bn.mpe_ask import mpe_ask
+from project_bn.map_ask import map_ask
 from bif_serializer import parse_bif_spec
+from project_bn.benchmark import benchmark
 import os
 
 
@@ -8,8 +10,15 @@ def main():
     # dir_path = os.path.dirname(os.path.realpath(__file__))
     # print(dir_path)
     net = BayesNet(parse_bif_spec("resources/earthquake.xml"))
+
     # burglary_ = burglary
-    ris = mpe_ask(dict(Alarm=True), net)
+
+    with benchmark("ciao") as bench:
+        ris = mpe_ask(dict(Alarm=True), net)
+
+    for pars in param:
+        mpe_ask(*pars)
+        map_ask()
 
     print(ris.show_approx())
 
