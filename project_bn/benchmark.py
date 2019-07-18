156dc861ee3ad19863cfda6aad0518ce6feb8165
heygent <heygent@users.noreply.github.com>
2019-07-18 18:20:37 +0200

    Aggiunge tool per benchmark

---
+++ project_bn/benchmark.py
@@ -0,0 +1,27 @@
+import time
+import gc
+from dataclasses import dataclass
+from contextlib import contextmanager
+
+
+@dataclass()
+class Benchmark:
+    name: str
+    start: float
+    end: float
+
+    @property
+    def duration_ms(self):
+        return (self.end - self.start) / 1000000
+
+
+@contextmanager
+def benchmark(name, print_result=True):
+    b = Benchmark(name, 0, 0)
+    gc.disable()
+    b.start = time.perf_counter_ns()
+    yield b
+    b.end = time.perf_counter_ns()
+    gc.enable()
+    if print_result:
+        print(f'Benchmark "{name}" took {b.duration_ms} ms')
