fdfcfdc6b1e69566bc78ac31ecbf38b304d75c96
acptIA <acpt1994@gmail.com>
2019-07-19 18:33:52 +0200

    analisi

--- project_bn/benchmark.py
+++ project_bn/benchmark.py
@@ -11,17 +11,17 @@ class Benchmark:
     end: float
 
     @property
-    def duration_ms(self):
-        return (self.end - self.start) / 1000000
+    def duration_s(self):
+        return (self.end - self.start) / 1000000000
 
 
 @contextmanager
 def benchmark(name, print_result=True):
     b = Benchmark(name, 0, 0)
-    gc.disable()
+    # gc.disable()
     b.start = time.perf_counter_ns()
     yield b
     b.end = time.perf_counter_ns()
-    gc.enable()
-    if print_result:
-        print(f'Benchmark "{name}" took {b.duration_ms} ms')
+    # gc.enable()
+    # if print_result:
+    # print(f'Benchmark "{name}" took {b.duration_s} s')
