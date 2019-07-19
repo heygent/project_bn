928842014f97871eaf90fe9225a99f1c1b8430fc
heygent <heygent@users.noreply.github.com>
2019-07-19 11:32:25 +0200

    Algo alternativo per mpe

--- project_bn/mpe_ask.py
+++ project_bn/mpe_ask.py
@@ -1,13 +1,19 @@
 from collections import Counter
 from typing import List
 
-from aima_probability import BayesNet, Factor, make_factor
-from project_bn.maxout_factor import MaxoutFactor
+from aima_probability import (
+    BayesNet,
+    Factor,
+    make_factor,
+    pointwise_product,
+    is_hidden,
+)
+from project_bn.maxout_factor import MaxoutFactor, make_maxout_factor
 
 
-def mpe_ask(e, bn: BayesNet):
-    factors = [make_factor(v, e, bn) for v in bn.variables]
-    return mpe_ask_factors(factors, bn)
+# def mpe_ask(e, bn: BayesNet):
+#     factors = [make_factor(v, e, bn) for v in bn.variables]
+#     return mpe_ask_factors(factors, bn)
 
 
 def mpe_ask_factors(factors: List[Factor], bn):
@@ -37,4 +43,54 @@ def mpe_ask_factors(factors: List[Factor], bn):
             break
 
     # noinspection PyUnboundLocalVariable
+    return factor.previous_assignments[()], factor.cpt[()]
+
+#
+# def mpe_ask(e, bn: BayesNet):
+#     factors = make_maxout_factor(bn.variables[-1], e, bn)
+#     var_count = Counter(var for f in factors for var in f.variables)
+#     multiplied = {bn.variables[-1]}
+#     to_max_out = set(bn.variables) - e.keys()
+#
+#     for var in reversed(bn.variables[:-1]):
+#
+#         new_factor = make_maxout_factor(var, e, bn)
+#         factor = factor.pointwise_product(new_factor, bn)
+#
+#         multiplied.add(var)
+#
+#         if multiplied.issuperset(factor.variables):
+#             for v in multiplied.intersection(factor.variables, to_max_out):
+#                 if len(to_max_out) == 1:
+#                     break
+#                 factor = factor.max_out(v, bn)
+#                 to_max_out.remove(v)
+#         if len(to_max_out) == 1:
+#             break
+#
+#     return factor.previous_assignments[()], factor.cpt[()]
+
+
+def max_out(var, factors, bn):
+    """Eliminate var from all factors by summing over its values."""
+    result, var_factors = [], []
+    for f in factors:
+        if var in f.variables:
+            var_factors.append(f)
+        else:
+            result.append(f)
+    result.append(pointwise_product(var_factors, bn).max_out(var, bn))
+    return result
+
+
+def mpe_ask(e, bn):
+    factors = [
+        make_maxout_factor(var, e, bn) for var in reversed(bn.variables)
+    ]
+    to_max_out = [var for var in reversed(bn.variables) if var not in e]
+
+    for var in to_max_out:
+        factors = max_out(var, factors, bn)
+
+    factor = factors[0]
     return factor.previous_assignments[()], factor.cpt[()]
