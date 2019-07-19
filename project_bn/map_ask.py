3fc04d1688a939ccff4cd18451cfea2813776597
heygent <heygent@users.noreply.github.com>
2019-07-19 11:50:22 +0200

    nuovo algoritmo per map

--- project_bn/map_ask.py
+++ project_bn/map_ask.py
@@ -1,7 +1,8 @@
 from typing import List, Tuple
 
 from aima_probability import BayesNet, is_hidden, make_factor, sum_out
-from project_bn.mpe_ask import mpe_ask_factors
+from project_bn.maxout_factor import MaxoutFactor
+from project_bn.mpe_ask import maxout_all
 
 
 def map_ask(Ms: List[str], e: dict, bn: BayesNet) -> Tuple[dict, float]:
@@ -11,4 +12,4 @@ def map_ask(Ms: List[str], e: dict, bn: BayesNet) -> Tuple[dict, float]:
         if is_hidden(var, Ms, e):
             factors = sum_out(var, factors, bn)
 
-    return mpe_ask_factors(factors, bn)
+    return maxout_all([MaxoutFactor(f) for f in factors], Ms, bn)
--- project_bn/mpe_ask.py
+++ project_bn/mpe_ask.py
@@ -1,74 +1,16 @@
-from collections import Counter
-from typing import List
-
 from aima_probability import (
     BayesNet,
-    Factor,
-    make_factor,
     pointwise_product,
-    is_hidden,
 )
-from project_bn.maxout_factor import MaxoutFactor, make_maxout_factor
+from project_bn.maxout_factor import make_maxout_factor
 
 
-# def mpe_ask(e, bn: BayesNet):
-#     factors = [make_factor(v, e, bn) for v in bn.variables]
-#     return mpe_ask_factors(factors, bn)
-
-
-def mpe_ask_factors(factors: List[Factor], bn):
-    factors = [MaxoutFactor(f) for f in factors]
-    var_count = Counter(var for f in factors for var in f.variables)
-
-    assert len(factors) >= 1
-
-    while factors:
-        factor = factors.pop()
-
-        for var in factor.variables:
-            # Se la variabile è contenuta solo in quel fattore, posso fare
-            # il maxout.
-            if var_count[var] == 1:
-                factor = factor.max_out(var, bn)
-                var_count[var] -= 1
-
-        if factors:
-            to_multiply = factors.pop()
-            var_count -= Counter(
-                set(factor.variables).intersection(to_multiply.variables)
-            )
-            factor = factor.pointwise_product(to_multiply, bn)
-            factors.append(factor)
-        else:
-            break
-
-    # noinspection PyUnboundLocalVariable
-    return factor.previous_assignments[()], factor.cpt[()]
-
-#
-# def mpe_ask(e, bn: BayesNet):
-#     factors = make_maxout_factor(bn.variables[-1], e, bn)
-#     var_count = Counter(var for f in factors for var in f.variables)
-#     multiplied = {bn.variables[-1]}
-#     to_max_out = set(bn.variables) - e.keys()
-#
-#     for var in reversed(bn.variables[:-1]):
-#
-#         new_factor = make_maxout_factor(var, e, bn)
-#         factor = factor.pointwise_product(new_factor, bn)
-#
-#         multiplied.add(var)
-#
-#         if multiplied.issuperset(factor.variables):
-#             for v in multiplied.intersection(factor.variables, to_max_out):
-#                 if len(to_max_out) == 1:
-#                     break
-#                 factor = factor.max_out(v, bn)
-#                 to_max_out.remove(v)
-#         if len(to_max_out) == 1:
-#             break
-#
-#     return factor.previous_assignments[()], factor.cpt[()]
+def mpe_ask(e, bn: BayesNet):
+    factors = [
+        make_maxout_factor(var, e, bn) for var in reversed(bn.variables)
+    ]
+    to_max_out = [var for var in reversed(bn.variables) if var not in e]
+    return maxout_all(factors, to_max_out, bn)
 
 
 def max_out(var, factors, bn):
@@ -83,11 +25,7 @@ def max_out(var, factors, bn):
     return result
 
 
-def mpe_ask(e, bn):
-    factors = [
-        make_maxout_factor(var, e, bn) for var in reversed(bn.variables)
-    ]
-    to_max_out = [var for var in reversed(bn.variables) if var not in e]
+def maxout_all(factors, to_max_out, bn):
 
     for var in to_max_out:
         factors = max_out(var, factors, bn)
